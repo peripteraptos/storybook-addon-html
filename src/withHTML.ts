d6a3d058854047854c47ff79e9347ba6c2a6d28f
Benedikt Ackermann <benacke@gmail.com>
2025-11-08 21:52:29 +0100

    removed setTimeout

--- src/withHTML.ts
+++ src/withHTML.ts
@@ -14,30 +14,28 @@ export const withHTML = (
 ) => {
   const emit = useChannel({});
 
-  setTimeout(() => {
-    const rootSelector = parameters.root || "#storybook-root, #root";
-    const root = document.querySelector(rootSelector);
-    let code: string = root ? root.innerHTML : `${rootSelector} not found.`;
-    const { removeEmptyComments, removeComments, transform } = parameters;
-    if (removeEmptyComments) {
-      code = code.replace(/<!--\s*-->/g, "");
+  const rootSelector = parameters.root || "#storybook-root, #root";
+  const root = document.querySelector(rootSelector);
+  let code: string = root ? root.innerHTML : `${rootSelector} not found.`;
+  const { removeEmptyComments, removeComments, transform } = parameters;
+  if (removeEmptyComments) {
+    code = code.replace(/<!--\s*-->/g, "");
+  }
+  if (removeComments === true) {
+    code = code.replace(/<!--[\S\s]*?-->/g, "");
+  } else if (removeComments instanceof RegExp) {
+    code = code.replace(/<!--([\S\s]*?)-->/g, (match, p1) =>
+      removeComments.test(p1) ? "" : match,
+    );
+  }
+  if (typeof transform === "function") {
+    try {
+      code = transform(code);
+    } catch (e) {
+      console.error(e);
     }
-    if (removeComments === true) {
-      code = code.replace(/<!--[\S\s]*?-->/g, "");
-    } else if (removeComments instanceof RegExp) {
-      code = code.replace(/<!--([\S\s]*?)-->/g, (match, p1) =>
-        removeComments.test(p1) ? "" : match,
-      );
-    }
-    if (typeof transform === "function") {
-      try {
-        code = transform(code);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    emit(EVENTS.CODE_UPDATE, { code, options: parameters });
-  }, 0);
+  }
+  emit(EVENTS.CODE_UPDATE, { code, options: parameters });
 
   return storyFn();
 };
